e117e5d267c3c33c82ceeee7197d0643f19dc0c6
4lpine <112321074+4lpine@users.noreply.github.com>
2023-06-09 20:34:13 +0530

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -22,22 +22,26 @@ borders_width = 5
 message_color = "#202020"
 message_height = 30
 better_font = ("Consolas", 24)
-wraplength=80
-padx=2
-pady=2
+wraplength = 80
+padx = 2
+pady = 2
 
 secret_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 s = socket.socket()
 
+
 def encrypt(message):
     nonce = urandom(12)
-    cipher = AES.new(secret_key.encode('utf-8'), AES.MODE_GCM, nonce=nonce)
-    ciphertext, tag = cipher.encrypt_and_digest(pad(message.encode('utf-8'), AES.block_size))
-    ciphertext_b64 = b64encode(ciphertext).decode('utf-8')
-    nonce_b64 = b64encode(nonce).decode('utf-8')
-    tag_b64 = b64encode(tag).decode('utf-8')
+    cipher = AES.new(secret_key.encode("utf-8"), AES.MODE_GCM, nonce=nonce)
+    ciphertext, tag = cipher.encrypt_and_digest(
+        pad(message.encode("utf-8"), AES.block_size)
+    )
+    ciphertext_b64 = b64encode(ciphertext).decode("utf-8")
+    nonce_b64 = b64encode(nonce).decode("utf-8")
+    tag_b64 = b64encode(tag).decode("utf-8")
 
-    return '~'.join([ciphertext_b64, nonce_b64, tag_b64])
+    return "~".join([ciphertext_b64, nonce_b64, tag_b64])
+
 
 def decrypt(encrypted_message):
     deconstructed_encrypted_message = encrypted_message.split("~")
@@ -50,6 +54,7 @@ def decrypt(encrypted_message):
 
     return plaintext
 
+
 def receive_messages(message_panel, slider_panel):
     while True:
         data = s.recv(buffer).decode()
@@ -61,12 +66,18 @@ def receive_messages(message_panel, slider_panel):
                         child.destroy()
 
                 name_list = data.removeprefix("[NAME_LIST]").split("ㅤ")
-                
+
                 for member in name_list:
                     name = decrypt(member)
-                    display_name = CTkButton(master=slider_panel, text=name, font=better_font, fg_color=message_color, height=32)
-                    display_name.pack(side=TOP, fill='x', padx=padx, pady=pady)
-            
+                    display_name = CTkButton(
+                        master=slider_panel,
+                        text=name,
+                        font=better_font,
+                        fg_color=message_color,
+                        height=32,
+                    )
+                    display_name.pack(side=TOP, fill="x", padx=padx, pady=pady)
+
             elif data.startswith("[CHAT_LOG]"):
                 chat_log_length = int(data.removeprefix("[CHAT_LOG]"))
                 s.send(str.encode("received"))
@@ -74,30 +85,83 @@ def receive_messages(message_panel, slider_panel):
                     for i in range(int(chat_log_length)):
                         message = decrypt(s.recv(1024).decode()).split(" : ")
                         message_header = message[0].lstrip()
-                        lines = [message[1].lstrip()[i:i+wraplength] for i in range(0, len(message[1].lstrip()), wraplength)]
-                        display_messages = CTkButton(master=message_panel, text=message_header + " : {}\n{}".format(lines[0], indent('\n'.join(lines[1:]), ' ' * (len(message_header) + 2))), font=better_font, anchor="w", fg_color=message_color, height=message_height, text_color="silver")
-                        display_messages.pack(side=TOP, fill='x', padx=padx, pady=pady)
+                        lines = [
+                            message[1].lstrip()[i : i + wraplength]
+                            for i in range(0, len(message[1].lstrip()), wraplength)
+                        ]
+                        display_messages = CTkButton(
+                            master=message_panel,
+                            text=message_header
+                            + " : {}\n{}".format(
+                                lines[0],
+                                indent(
+                                    "\n".join(lines[1:]),
+                                    " " * (len(message_header) + 2),
+                                ),
+                            ),
+                            font=better_font,
+                            anchor="w",
+                            fg_color=message_color,
+                            height=message_height,
+                            text_color="silver",
+                        )
+                        display_messages.pack(side=TOP, fill="x", padx=padx, pady=pady)
                         s.send(str.encode("received"))
             else:
                 message = decrypt(data).split(" : ")
                 message_header = message[0].lstrip()
-                lines = [message[1].lstrip()[i:i+wraplength] for i in range(0, len(message[1].lstrip()), wraplength)]
-                display_messages = CTkButton(master=message_panel, text=message_header + " : {}\n{}".format(lines[0], indent('\n'.join(lines[1:]), ' ' * (len(message_header) + 2))), font=better_font, anchor="nw", fg_color=message_color, height=message_height, text_color="silver")
-                display_messages.pack(side=TOP, fill='x', padx=padx, pady=pady)
-                
-            
+                lines = [
+                    message[1].lstrip()[i : i + wraplength]
+                    for i in range(0, len(message[1].lstrip()), wraplength)
+                ]
+                display_messages = CTkButton(
+                    master=message_panel,
+                    text=message_header
+                    + " : {}\n{}".format(
+                        lines[0],
+                        indent("\n".join(lines[1:]), " " * (len(message_header) + 2)),
+                    ),
+                    font=better_font,
+                    anchor="nw",
+                    fg_color=message_color,
+                    height=message_height,
+                    text_color="silver",
+                )
+                display_messages.pack(side=TOP, fill="x", padx=padx, pady=pady)
+
+
 def send(message, message_panel, username, entry):
     entry.delete(0, END)
     if message != "":
         s.send(str.encode(encrypt(f"{username} : {message}")))
-        lines = [message[i:i+wraplength] for i in range(0, len(message), wraplength)]
-        display_message = CTkButton(master=message_panel, text="You : {}\n{}".format(lines[0], indent('\n'.join(lines[1:]), ' ' * 6)), font=better_font, anchor="nw", fg_color=message_color, height=message_height)
-        display_message.pack(side=TOP, fill='x', padx=padx, pady=pady)
-    
+        lines = [
+            message[i : i + wraplength] for i in range(0, len(message), wraplength)
+        ]
+        display_message = CTkButton(
+            master=message_panel,
+            text="You : {}\n{}".format(lines[0], indent("\n".join(lines[1:]), " " * 6)),
+            font=better_font,
+            anchor="nw",
+            fg_color=message_color,
+            height=message_height,
+        )
+        display_message.pack(side=TOP, fill="x", padx=padx, pady=pady)
+
+
 def send_messages(send_message_panel, message_panel, username):
-    entry = CTkEntry(master=send_message_panel, height=50, width=1500, placeholder_text="Send", corner_radius=20, font=better_font)
+    entry = CTkEntry(
+        master=send_message_panel,
+        height=50,
+        width=1500,
+        placeholder_text="Send",
+        corner_radius=20,
+        font=better_font,
+    )
     entry.place(relx=0.5, rely=0.5, anchor=CENTER)
-    entry.bind('<Return>', lambda event: send(entry.get(), message_panel, username, entry))
+    entry.bind(
+        "<Return>", lambda event: send(entry.get(), message_panel, username, entry)
+    )
+
 
 def main_gui(username):
     root = CTk()
@@ -107,38 +171,72 @@ def main_gui(username):
     root.rowconfigure(index=0, weight=5)
     root.rowconfigure(index=1, weight=1)
 
-    message_panel = CTkScrollableFrame(master=root, border_color=borders, border_width=borders_width)
+    message_panel = CTkScrollableFrame(
+        master=root, border_color=borders, border_width=borders_width
+    )
     message_panel.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
 
-    slider_panel = CTkScrollableFrame(master=root, border_color=borders, border_width=borders_width, label_text="Online Users : ", label_font=('Consolas', 26), label_anchor="nw", label_fg_color="#2B2B2B")
+    slider_panel = CTkScrollableFrame(
+        master=root,
+        border_color=borders,
+        border_width=borders_width,
+        label_text="Online Users : ",
+        label_font=("Consolas", 26),
+        label_anchor="nw",
+        label_fg_color="#2B2B2B",
+    )
     slider_panel.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
 
-    send_message_panel = CTkFrame(master=root, height=10, border_color=borders, border_width=borders_width)
-    send_message_panel.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=20, pady=20)
-    
+    send_message_panel = CTkFrame(
+        master=root, height=10, border_color=borders, border_width=borders_width
+    )
+    send_message_panel.grid(
+        row=1, column=0, columnspan=2, sticky="nsew", padx=20, pady=20
+    )
+
     s.connect((server, port))
     s.send(str.encode(encrypt(username)))
 
     Thread(target=receive_messages, args=(message_panel, slider_panel)).start()
-    Thread(target=send_messages, args=(send_message_panel, message_panel, username)).start()
+    Thread(
+        target=send_messages, args=(send_message_panel, message_panel, username)
+    ).start()
 
     root.mainloop()
+
 
 def initiate_main_gui(username):
     if username != "":
         root.destroy()
-        main_gui(username)    
+        main_gui(username)
+
 
 def startup():
     frame = CTkFrame(master=root, border_color=borders, border_width=borders_width)
     frame.pack(padx=100, pady=100, fill=BOTH, expand=True)
-    
-    entry = CTkEntry(master=frame, height=50, width=300, placeholder_text="Name", corner_radius=20, font=better_font)
+
+    entry = CTkEntry(
+        master=frame,
+        height=50,
+        width=300,
+        placeholder_text="Name",
+        corner_radius=20,
+        font=better_font,
+    )
     entry.place(relx=0.5, rely=0.5, anchor=CENTER)
-    entry.bind('<Return>', lambda event: initiate_main_gui(entry.get()))
-    
-    button = CTkButton(master=frame, text="Enter :)", height=50, width=300, corner_radius=20, font=better_font, command = lambda : initiate_main_gui(entry.get()))
+    entry.bind("<Return>", lambda event: initiate_main_gui(entry.get()))
+
+    button = CTkButton(
+        master=frame,
+        text="Enter :)",
+        height=50,
+        width=300,
+        corner_radius=20,
+        font=better_font,
+        command=lambda: initiate_main_gui(entry.get()),
+    )
     button.place(relx=0.5, rely=0.65, anchor=CENTER)
+
 
 startup()
 root.mainloop()
